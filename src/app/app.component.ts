8ab4db1ab07ef3feb5a04c3ad9909d19de2feabe
dor <dorzil1998@gmail.com>
2020-07-16 12:53:14 +0300

    [DEPLOY] - added log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,25 +26,25 @@ export class AppComponent {
           return prop;
         }
       });
-      let id = ans.val.split('@')[0];
-      this._postgres.getJobByID(this._context.serverURL, id).then((answer) => {
-        if (answer.jobTitle === 'אפסנאי') {
-          this.userService.setUserID(id);
-          if (this._router.url == '/orders') {
-            this.startRefreshEvent = new CustomEvent('refreshStart');
-            this.endRefreshEvent = new CustomEvent('refreshEnd');
-            dispatchEvent(this.startRefreshEvent);
-            this.onRefresh();
-          } else {
-            this._appState.pageNotFound = true;
-          }
-        } else if (!(this._router.url == "/404")) {
-          this._appState.pageNotAuthorized = true;
-          this._router.navigate(['403']);
+    let id = ans.val.split('@')[0];
+    this._postgres.getJobByID(this._context.serverURL, id).then((answer) => {
+      if (answer.jobTitle === 'אפסנאי') {
+        this.userService.setUserID(id);
+        if (this._router.url == '/orders') {
+          this.startRefreshEvent = new CustomEvent('refreshStart');
+          this.endRefreshEvent = new CustomEvent('refreshEnd');
+          dispatchEvent(this.startRefreshEvent);
+          this.onRefresh();
         } else {
           this._appState.pageNotFound = true;
         }
-      });
+      } else if (!(this._router.url == "/404")) {
+        this._appState.pageNotAuthorized = true;
+        this._router.navigate(['403']);
+      } else {
+        this._appState.pageNotFound = true;
+      }
+    });
     });
   }
 
@@ -67,6 +67,10 @@ export class AppComponent {
     this.onRefresh();
   }
 
+  onLogOut() {
+    window.location.href = 'https://storageapp.azurewebsites.net/.auth/logout';
+  }
+
   waitingFilter(item) {
     return (item.status == "waiting") ? true : false;
   }
@@ -74,5 +78,13 @@ export class AppComponent {
   readyFilter(item) {
     return (item.status != "waiting") ? true : false;
 
+  }
+  expandBtn(){
+    document.getElementById('logOutIcon').classList.add('icon-opened')
+    document.getElementById('logOutBtn').classList.add('btn-opened')
+  }
+  collapseBtn(){
+    document.getElementById('logOutIcon').classList.remove('icon-opened')
+    document.getElementById('logOutBtn').classList.remove('btn-opened')
   }
 }
